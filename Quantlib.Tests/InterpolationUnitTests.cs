8b1d21a2a7430cdedd0af1d9a2661a28cdcb73c8
Lantsov Aleksander <lantsov.aleksander@gmail.com>
2021-09-24 01:49:43 +0300

    Implementing linear interpolation + adding unit test for it

---
+++ Quantlib.Tests/InterpolationUnitTests.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Quantlib.Tests
+{
+    public class InterpolationUnitTests
+    {
+
+        private readonly LinearInterpolator _interpolator = new LinearInterpolator();
+        
+        [Fact]
+        public void test_Baseline_Interpolation()
+        {
+            var testData = new SortedList<double, double>
+            {
+                {0.0d, 9.0d},
+                {1.0d, 1.0d},
+                {2.0d, 7.0d},
+                {4.0d, 1.0d}
+            };
+
+            Assert.Equal(4.0d, _interpolator.Calculate(testData, 1.5d), 6);
+        }
+        
+        [Fact]
+        public void test_Baseline_ExactMatch()
+        {
+            var testData = new SortedList<double, double>
+            {
+                {0.0d, 9.0d},
+                {1.0d, 1.0d},
+                {2.0d, 7.0d},
+                {4.0d, 1.0d}
+            };
+
+            Assert.Equal(1.0d, _interpolator.Calculate(testData, 4.0d), 6);
+        }
+        
+        [Fact]
+        public void test_No_Extrapolation_Left()
+        {
+            var testData = new SortedList<double, double>
+            {
+                {0.0d, 9.0d},
+                {1.0d, 1.0d},
+                {2.0d, 7.0d},
+                {4.0d, 1.0d}
+            };
+
+            Assert.Throws<ArgumentException>(() => _interpolator.Calculate(testData, -1.0d));
+        }        
+        
+        [Fact]
+        public void test_No_Extrapolation_Right()
+        {
+            var testData = new SortedList<double, double>
+            {
+                {0.0d, 9.0d},
+                {1.0d, 1.0d},
+                {2.0d, 7.0d},
+                {4.0d, 1.0d}
+            };
+
+            Assert.Throws<ArgumentException>(() => _interpolator.Calculate(testData, 5.0d));
+        }        
+        
+        [Fact]
+        public void test_Empty()
+        {
+            var testData = new SortedList<double, double>();
+
+            Assert.Throws<ArgumentException>(() => _interpolator.Calculate(testData, 1.0d));
+        }
+        
+        [Fact]
+        public void test_OnePoint_ExactMatch()
+        {
+            var testData = new SortedList<double, double>()
+            {
+                {1.0d, 14.0d}
+            };
+            
+            Assert.Equal(14.0d, _interpolator.Calculate(testData, 1.0d), 6);
+        }
+
+        [Fact]
+        public void test_OnePoint_DontMatch()
+        {
+            var testData = new SortedList<double, double>()
+            {
+                {1.0d, 14.0d}
+            };
+            
+            Assert.Throws<ArgumentException>(() => _interpolator.Calculate(testData, 3.0d));
+        }
+    }
+}
---
+++ Quantlib/Interpolation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace Quantlib
+{
+    public interface IInterpolator
+    {
+        double Calculate(SortedList<double, double> data, double targetX);
+    }
+
+    public class LinearInterpolator : IInterpolator
+    {
+        [Pure]
+        public double Calculate(SortedList<double, double> data, double targetX)
+        {
+            if (!data.Any() || data.Keys.First() > targetX || data.Keys.Last() < targetX)
+            {
+                throw new ArgumentException("Bad arguments");
+            }
+
+            if (data.Count == 1)
+            {
+                return data.Values.First();
+            }
+
+            var index = data.Keys.ToList().BinarySearch(targetX);
+
+            if (index >= 0)
+            {
+                return data.ElementAt(index).Value;
+            }
+
+            index = ~index;
+
+            var (x0, y0) = data.ElementAt(index - 1);
+            var (x1, y1) = data.ElementAt(index);
+            
+            return y0 + (targetX - x0) * (y1 - y0) / (x1 - x0);
+        }
+    }
+}
